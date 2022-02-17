b62ed3d02de29029b357b44d5f1c2a44ce1a44b2
tomzso <99918734+tomzso@users.noreply.github.com>
2022-02-17 22:12:35 +0100

    Add files via upload

---
+++ lab_01-master-62317cfbf66f7f6f3c5c0c2d97f6b9a5b9524e0c/nagyobb/fuggvenyeim.cpp
@@ -0,0 +1,23 @@
+// Fájl: fuggvenyeim.cpp
+// Ebben valósítom meg a gyakran használt függvényeket.
+
+#include "fuggvenyeim.h"
+
+// Két int adat felcserélése
+void csere(int& a, int& b) {
+   int tmp =  a;
+   a =  b;
+   b = tmp;
+
+
+}
+
+double polinom(double x, double const *a, int n) {
+    double y = 0;
+    for (int i = n ; i >= 0; i--) {
+        y = y * x + a[i];
+    }
+    return y;
+}
+
+// ....
