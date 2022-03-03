a5411b01fdb01285ca74e127c4b46826ddb9f3d0
tomzso <99918734+tomzso@users.noreply.github.com>
2022-03-03 11:09:32 +0100

    Add files via upload

---
+++ CppEllenallas-master-85c62e31df0980442ecf9362243204c341f78302/CppEllenallas-master-85c62e31df0980442ecf9362243204c341f78302/resistor.cpp
@@ -0,0 +1,54 @@
+﻿/**
+ * \file resistor.cpp
+ *   (UTF-8 kodolasu fajl. Allitsa at a megjenetes kodolasat,
+ *    ha a tovabbi kommentek nem olvashatok helyesen!)
+ *
+ * Ohmikus ellenállást modellező osztály megvalósítása
+ */
+
+/**
+ * Itt kell megvalósítani a resistor.h-ban deklarált nem inline függvényeket.
+ * A Jportára ezt a fájlt kell feltölteni.
+ */
+
+#include "resistor.h"
+
+double Resistor::defR=126;
+
+
+Resistor::Resistor(double r){
+    R=r;
+}
+
+Resistor::Resistor(){
+    R=defR;
+}
+
+void Resistor::setDef(double r){
+    defR=r;
+}
+
+
+Resistor Resistor::operator+(const Resistor& r) const{
+    return Resistor(R+r.R);
+}
+
+Resistor Resistor::operator%(const Resistor& r) const{
+    return Resistor(1/((1/R)+1/r.R));
+}
+
+Resistor operator*(int n, const Resistor& r){
+    if(n<=0)
+        throw "V6AO9G";
+
+    double sum;
+    for(int i=0;i<n;i++){
+        sum=sum+r.getR();
+
+    }
+    return Resistor(sum);
+}
+
+
+
+
