609507c346aee8e7ab70bf41d95f088ebae1df04
tomzso <99918734+tomzso@users.noreply.github.com>
2022-03-03 11:08:13 +0100

    Add files via upload

---
+++ lab_05-master-9530f564588a02fb42fb5a584b5bdb5e3a2042eb/string5.cpp
@@ -0,0 +1,59 @@
+/**
+ *
+ * \file string5.cpp
+ *
+ * Itt kell megvalósítania a hiányzó tagfüggvényeket.
+ * Segítségül megadtuk a C++ nem OO eszközeinek felhasználásával készített String-kezelő
+ * függvényke neveit.
+ *
+ * Ha valamit INLINE-ként valósít meg, akkor annak a string5.h-ba kell kerülnie,
+ * akár kívül akár osztályon belül definiálja. (Az inline függvényeknek minden
+ * fordítási egységben elérhetőknek kell lenniük)
+ * *
+ * A teszteléskor ne felejtse el beállítani a string5.h állományban az ELKESZULT makrót.
+ *
+ */
+
+#ifdef _MSC_VER
+// MSC ne adjon figyelmeztető üzenetet a C sztringkezelő függvényeire
+  #define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include <iostream>             // Kiíratáshoz
+#include <cstring>              // Sztringműveletekhez
+
+#include "memtrace.h"           // a standard headerek után kell lennie
+#include "string5.h"
+
+
+/// Konstruktorok: egy char karakterből (createString)
+///                egy nullával lezárt char sorozatból (createString)
+
+
+
+
+/// Másoló konstruktor: String-ből készít (createString)
+
+/// Destruktor (disposeString)
+
+/// operator=
+
+/// [] operátorok: egy megadott indexű elem REFERENCIÁJÁVAL térnek vissza (charAtString)
+/// indexhiba esetén const char * kivételt dob!
+
+
+/// + operátorok:
+///                 String-hez jobbról karaktert ad (addString)
+///                 String-hez String-et ad (addString)
+
+String operator+(char a, const String& rhs) {
+
+    String c=String(a)+rhs;
+
+    return c;
+}
+
+/// << operator, ami kiír az ostream-re
+
+
+/// >> operátor, ami beolvas az istream-ről egy szót
